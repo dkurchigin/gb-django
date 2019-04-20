0da160cb4abd14a4d71c7db4de631e9ee48771ba
Dkurchigin <kurchigin.dmitry@yandex.ru>
2019-04-20 22:46:10 +0300

    add first lesson

---
+++ gb_lesson1_djgo/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dk\desktop\django_dir\gb_lessons_django\first_lesson\gb_lesson1_djgo\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
