57e288c7c8ef5aaaac4815aeec14ea2caa98de37
Dkurchigin <kurchigin.dmitry@yandex.ru>
2019-04-25 21:08:50 +0300

    second lesson django

--- gb_lesson1_djgo/gb_lesson1_djgo/urls.py
+++ gb_lesson1_djgo/gb_lesson1_djgo/urls.py
@@ -18,8 +18,8 @@ from django.urls import path
 import mainapp.views as mainapp
 
 urlpatterns = [
-    path('', mainapp.main),
-    path('products', mainapp.products),
-    path('contacts', mainapp.contacts),
-    path('admin/', admin.site.urls),
+    path('', mainapp.main, name='main'),
+    path('products', mainapp.products, name='products'),
+    path('contacts', mainapp.contacts, name='contacts'),
+    path('admin/', admin.site.urls, name='admin'),
 ]
